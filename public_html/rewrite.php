4de7766cb028a59e532796b9dbc0c7afaef8724e
Sven Slootweg <jamsoftgamedev@gmail.com>
2013-07-07 01:40:21 +0200

    Bootstrap stuff

---
+++ public_html/rewrite.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * openNG is more free software. It is licensed under the WTFPL, which
+ * allows you to do pretty much anything with it, without having to
+ * ask permission. Commercial use is allowed, and no attribution is
+ * required. We do politely request that you share your modifications
+ * to benefit other developers, but you are under no enforced
+ * obligation to do so :)
+ * 
+ * Please read the accompanying LICENSE document for the full WTFPL
+ * licensing text.
+ */
+
+$_APP = true;
+require("includes/base.php");
+
+$router = new CPHPRouter();
+
+$router->ignore_query = true;
+$router->allow_slash = true;
+
+$router->routes = array(
+	0 => array(
+		"^/$"		=> "modules/index.php"
+	)
+);
+
+$router->RouteRequest();
